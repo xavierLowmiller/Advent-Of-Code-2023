96b49dc6bb9e497cf23d3bc6ba797517cbeed74d
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-02 07:11:25 +0100

    Solve Day 2

--- Sources/Day2/Day2.swift
+++ Sources/Day2/Day2.swift
@@ -1,1 +1,74 @@
+struct Game {
+    var id: Int
+    var rounds: [Round]
 
+    var containsImpossibleRound: Bool {
+        rounds.contains(where: { !$0.isValid })
+    }
+
+    var cubePower: Int {
+        let set = rounds.minimalSetOfCubes
+        return set.red * set.green * set.blue
+    }
+
+    init!(_ input: Substring) {
+        guard let m = input.firstMatch(of: #/Game (\d+):(.*)\n?/#),
+              let id = Int(m.output.1)
+        else { return nil }
+
+        self.id = id
+        self.rounds = .init(m.output.2)
+    }
+}
+
+extension Game {
+    struct Round {
+        var red: Int
+        var green: Int
+        var blue: Int
+
+        var isValid: Bool {
+            guard red <= 12 else { return false }
+            guard green <= 13 else { return false }
+            guard blue <= 14 else { return false }
+            return true
+        }
+
+        init(_ input: Substring) {
+            red = input.firstMatch(of: #/ (\d+) red/#)
+                .flatMap { Int($0.output.1) } ?? 0
+            green = input.firstMatch(of: #/ (\d+) green/#)
+                .flatMap { Int($0.output.1) } ?? 0
+            blue = input.firstMatch(of: #/ (\d+) blue/#)
+                .flatMap { Int($0.output.1) } ?? 0
+        }
+    }
+}
+
+extension Array where Element == Game.Round {
+    init(_ s: Substring) {
+        self = s.split(separator: ";").map(Element.init)
+    }
+
+    var minimalSetOfCubes: (red: Int, green: Int, blue: Int) {
+        reduce((0, 0, 0)) {
+            (
+                Swift.max($0.0, $1.red),
+                Swift.max($0.1, $1.green),
+                Swift.max($0.2, $1.blue)
+            )
+        }
+    }
+}
+
+extension Collection where Element == Game {
+    var sumOfPossibleIDs: Int {
+        self
+            .filter { !$0.containsImpossibleRound }
+            .reduce(0) { $0 + $1.id }
+    }
+
+    var totalCubePower: Int {
+        self.map(\.cubePower).reduce(0, +)
+    }
+}
--- Tests/Day2Tests/Day2Tests.swift
+++ Tests/Day2Tests/Day2Tests.swift
@@ -2,15 +2,39 @@ import XCTest
 @testable import Day2
 
 final class Day2Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        let games = input.split(separator: "\n").compactMap(Game.init)
+        XCTAssertEqual(games.sumOfPossibleIDs, 8)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let games = input.split(separator: "\n").compactMap(Game.init)
+        print("Part 1:", games.sumOfPossibleIDs)
+    }
+
+    func testPart2Example() {
+        let input = """
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        """
+
+        let games = input.split(separator: "\n").compactMap(Game.init)
+        XCTAssertEqual(games.totalCubePower, 2286)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        let games = input.split(separator: "\n").compactMap(Game.init)
+        print("Part 2:", games.totalCubePower)
     }
 }
