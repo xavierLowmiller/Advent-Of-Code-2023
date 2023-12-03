ad0873a5a967d0cbed392e7ba368ca5702c47f6b
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-03 08:04:39 +0100

    Solve Day 3

--- Sources/Day3/Day3.swift
+++ Sources/Day3/Day3.swift
@@ -1,1 +1,88 @@
+struct Point: Hashable {
+    var x: Int
+    var y: Int
 
+    var adjacentPoints: Set<Point> {
+        [
+            Point(x: x - 1, y: y - 1),
+            Point(x: x - 1, y: y),
+            Point(x: x - 1, y: y + 1),
+            Point(x: x, y: y - 1),
+            Point(x: x, y: y + 1),
+            Point(x: x + 1, y: y - 1),
+            Point(x: x + 1, y: y),
+            Point(x: x + 1, y: y + 1),
+        ]
+    }
+
+    func gears(in partNumbers: Set<PartNumber>) -> (PartNumber, PartNumber)? {
+        let adjacentPoints = self.adjacentPoints
+        let eligibleNumbers: [PartNumber] = partNumbers.filter { !$0.points.intersection(adjacentPoints).isEmpty }
+        guard eligibleNumbers.count == 2 else { return nil }
+        return (eligibleNumbers[0], eligibleNumbers[1])
+    }
+}
+
+struct PartNumber: Hashable {
+    var points: Set<Point>
+    var value: Int
+
+    func isValidPartNumber(for symbols: Set<Point>) -> Bool {
+        symbols.map(\.adjacentPoints).contains { !$0.intersection(points).isEmpty }
+    }
+}
+
+struct Schematic {
+    var symbols: Set<Point> = []
+    var gears: Set<Point> = []
+    var partNumbers: Set<PartNumber> = []
+
+    init(_ input: String) {
+        for (y, line) in zip(0..., input.split(separator: "\n")) {
+            var number = 0
+            var coordinates: Set<Point> = []
+
+            func resetNumber() {
+                if number != 0 {
+                    partNumbers.insert(PartNumber(points: coordinates, value: number))
+                }
+                number = 0
+                coordinates = []
+            }
+
+            for (x, character) in zip(0..., line) {
+
+                if let n = character.wholeNumberValue {
+                    number *= 10
+                    number += n
+                    coordinates.insert(Point(x: x, y: y))
+                } else {
+                    resetNumber()
+
+                    if character == "." {
+                        continue
+                    } else {
+                        if character == "*" {
+                            gears.insert(Point(x: x, y: y))
+                        }
+                        symbols.insert(Point(x: x, y: y))
+                    }
+                }
+            }
+
+            resetNumber()
+        }
+    }
+
+    var sumOfEngineParts: Int {
+        partNumbers
+            .filter { $0.isValidPartNumber(for: symbols) }
+            .reduce(0) { $0 + $1.value }
+    }
+
+    var sumOfGearRatios: Int {
+        gears
+            .compactMap { $0.gears(in: partNumbers) }
+            .reduce(0) { $0 + $1.0.value * $1.1.value }
+    }
+}
--- Tests/Day3Tests/Day3Tests.swift
+++ Tests/Day3Tests/Day3Tests.swift
@@ -2,15 +2,49 @@ import XCTest
 @testable import Day3
 
 final class Day3Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        let schematic = Schematic(input)
+        XCTAssertEqual(schematic.sumOfEngineParts, 4361)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let schematic = Schematic(input)
+        print("Part 1:", schematic.sumOfEngineParts)
+    }
+
+    func testPart2Example() {
+        let input = """
+        467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..
+        """
+
+        let schematic = Schematic(input)
+        XCTAssertEqual(schematic.sumOfGearRatios, 467835)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        let schematic = Schematic(input)
+        print("Part 2:", schematic.sumOfGearRatios)
     }
 }
