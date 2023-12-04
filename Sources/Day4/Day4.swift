c349f65c6f318ae2a021e492425f22ed3ef7e308
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-04 08:18:12 +0100

    Solve Day 4

--- Sources/Day4/Day4.swift
+++ Sources/Day4/Day4.swift
@@ -1,1 +1,54 @@
+struct Card: Hashable, Identifiable {
+    let id: UInt
+    let winningNumbers: Set<UInt>
+    let numbers: Set<UInt>
+    let numberOfWinningNumbers: UInt
 
+    init?(_ input: Substring) {
+        guard let m = input.firstMatch(of: #/Card\s+(\d+):\s+([\d+\s+]+)\|\s+([\d+\s+]+)/#),
+              let id = UInt(m.output.1)
+        else { return nil }
+
+        self.id = id
+        winningNumbers = Set(m.output.2.split(omittingEmptySubsequences: true, whereSeparator: \.isWhitespace).compactMap { UInt($0) })
+        numbers = Set(m.output.3.split(omittingEmptySubsequences: true, whereSeparator: \.isWhitespace).compactMap { UInt($0) })
+        numberOfWinningNumbers = UInt(numbers.intersection(winningNumbers).count)
+    }
+
+    var score: UInt {
+        if numberOfWinningNumbers == 0 {
+            return 0
+        } else {
+            return 1 << (numberOfWinningNumbers - 1)
+        }
+    }
+}
+
+struct Cards {
+
+    let cards: [Card]
+
+    init(_ input: String) {
+        cards = input.split(separator: "\n").compactMap(Card.init)
+    }
+
+    var totalScore: UInt {
+        cards.reduce(0) { $0 + $1.score }
+    }
+
+    var numberOfTotalScratchCards: UInt {
+        var numberOfCards: [Card.ID: UInt] = [:]
+
+        for card in cards {
+            let id = card.id
+            let multiplier = numberOfCards[id, default: 1]
+            for x in (id + 1)..<(id + 1 + card.numberOfWinningNumbers) {
+                numberOfCards[x, default: 1] += multiplier
+            }
+        }
+
+        return cards.map(\.id).reduce(0) {
+            $0 + numberOfCards[$1, default: 1]
+        }
+    }
+}
--- Tests/Day4Tests/Day4Tests.swift
+++ Tests/Day4Tests/Day4Tests.swift
@@ -2,15 +2,43 @@ import XCTest
 @testable import Day4
 
 final class Day4Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        let cards = Cards(input)
+
+        XCTAssertEqual(cards.totalScore, 13)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let cards = Cards(input)
+        print("Part 1:", cards.totalScore)
+    }
+
+    func testPart2Example() {
+        let input = """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        """
+
+        let cards = Cards(input)
+
+        XCTAssertEqual(cards.numberOfTotalScratchCards, 30)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        let cards = Cards(input)
+        print("Part 2:", cards.numberOfTotalScratchCards)
     }
 }
