9ef2bd9c25ea509a75ff469453e4572145660589
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-06 09:12:25 +0100

    Solve Day 6

--- Sources/Day6/Day6.swift
+++ Sources/Day6/Day6.swift
@@ -1,1 +1,44 @@
+import Foundation
 
+struct Races {
+    let races: [(duration: Int, record: Int)]
+
+    init(_ input: String) {
+        let lines = input.split(separator: "\n")
+        let times = lines[0].split(whereSeparator: \.isWhitespace).dropFirst().compactMap { Int($0) }
+        let distances = lines[1].split(whereSeparator: \.isWhitespace).dropFirst().compactMap { Int($0) }
+        races = Array(zip(times, distances))
+    }
+
+    var waysToWin: Int {
+        races
+            .map(waysToBeatTheRecord)
+            .reduce(1, *)
+    }
+}
+
+struct Race {
+    let duration: Int
+    let record: Int
+
+    init(_ input: String) {
+        let lines = input.split(separator: "\n")
+        duration = Int(lines[0].split(whereSeparator: \.isWhitespace).dropFirst().joined())!
+        record = Int(lines[1].split(whereSeparator: \.isWhitespace).dropFirst().joined())!
+    }
+
+    var waysToWin: Int {
+        waysToBeatTheRecord(duration: duration, record: record)
+    }
+}
+
+private func waysToBeatTheRecord(duration: Int, record: Int) -> Int {
+    return (0...duration).reduce(into: 0) { total, i in
+        let remainingTime = duration - i
+        let distance = i * remainingTime
+
+        if distance > record {
+            total += 1
+        }
+    }
+}
--- Tests/Day6Tests/Day6Tests.swift
+++ Tests/Day6Tests/Day6Tests.swift
@@ -2,15 +2,35 @@ import XCTest
 @testable import Day6
 
 final class Day6Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        Time:      7  15   30
+        Distance:  9  40  200
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        let races = Races(input)
+        XCTAssertEqual(races.waysToWin, 288)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let races = Races(input)
+        XCTAssertEqual(races.waysToWin, 1108800)
+        print("Part 1:", races.waysToWin)
+    }
+
+    func testPart2Example() {
+        let input = """
+        Time:      7  15   30
+        Distance:  9  40  200
+        """
+
+        let race = Race(input)
+        XCTAssertEqual(race.waysToWin, 71503)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        let race = Race(input)
+        XCTAssertEqual(race.waysToWin, 36919753)
+        print("Part 2:", race.waysToWin)
     }
 }
