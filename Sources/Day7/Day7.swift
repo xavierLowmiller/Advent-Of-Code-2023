e2428c28cdabea798b0f1604a706649f46f72f52
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-07 22:36:55 +0100

    Solve Day 7

--- Sources/Day7/Day7.swift
+++ Sources/Day7/Day7.swift
@@ -1,1 +1,169 @@
+struct Hand: Comparable, CustomStringConvertible {
+    let cards: [Character]
+    let matchType: MatchType
+    let jIsJoker: Bool
 
+    enum MatchType: Comparable {
+        case fiveOfAKind
+        case fourOfAKind
+        case fullHouse
+        case threeOfAKind
+        case twoPair
+        case onePair
+        case highCard
+    }
+
+    init(_ input: Substring, jIsJoker: Bool = false) {
+        assert(input.count == 5)
+        let cards = Array(input)
+        self.cards = cards
+        if cards.fiveAreEqual(jIsJoker: jIsJoker) {
+            matchType = .fiveOfAKind
+        } else if cards.fourAreEqual(jIsJoker: jIsJoker) {
+            matchType = .fourOfAKind
+        } else if cards.isFullHouse(jIsJoker: jIsJoker) {
+            matchType = .fullHouse
+        } else if cards.threeAreEqual(jIsJoker: jIsJoker) {
+            matchType = .threeOfAKind
+        } else if cards.twoPairs(jIsJoker: jIsJoker) {
+            matchType = .twoPair
+        } else if cards.twoAreEqual(jIsJoker: jIsJoker) {
+            matchType = .onePair
+        } else if cards.allAreDifferent(jIsJoker: jIsJoker) {
+            matchType = .highCard
+        } else {
+            fatalError("Didn't match input: \(input)")
+        }
+        self.jIsJoker = jIsJoker
+    }
+
+    static func < (lhs: Self, rhs: Self) -> Bool {
+        if lhs.matchType == rhs.matchType {
+            let values = camelPokerValues(jIsJoker: lhs.jIsJoker)
+            return lhs.cards.map { values[$0]! } < rhs.cards.map { values[$0]! }
+        } else {
+            return lhs.matchType > rhs.matchType
+        }
+    }
+
+    var description: String {
+        String(cards)
+    }
+}
+
+struct Hands {
+    let hands: [(hand: Hand, bid: Int)]
+
+    init(_ input: String, jIsJoker: Bool = false) {
+        let hands = input.split(separator: "\n")
+        self.hands = hands.map {
+            (
+                hand: Hand($0.split(separator: " ")[0], jIsJoker: jIsJoker),
+                bid: Int($0.split(separator: " ")[1])!
+            )
+        }
+    }
+
+    var totalWinnings: Int {
+        hands
+            .sorted(by: { $0.hand < $1.hand })
+            .map(\.bid)
+            .enumerated()
+            .map { ($0 + 1) * $1 }
+            .reduce(0, +)
+    }
+}
+
+private extension Array where Element == Character {
+    private var numberOfJokers: Int {
+        filter { $0 == "J" }.count
+    }
+
+    private func dict(jIsJoker: Bool) -> [Character: Int] {
+        var dict = Dictionary(grouping: self) { $0 }.mapValues(\.count)
+        if jIsJoker {
+            dict.removeValue(forKey: "J")
+            let maxKey = dict.max(by: { $0.value < $1.value })?.key ?? "J"
+            dict[maxKey, default: 0] += numberOfJokers
+            return dict
+        } else {
+            return dict
+        }
+    }
+
+    func fiveAreEqual(jIsJoker: Bool) -> Bool {
+        return dict(jIsJoker: jIsJoker).count == 1
+    }
+
+    func fourAreEqual(jIsJoker: Bool) -> Bool {
+        let dict = self.dict(jIsJoker: jIsJoker)
+        return dict.count == 2
+            && dict.values.contains(where: { $0 == 4 })
+            && !dict.values.contains(where: { $0 == 3 })
+            && !dict.values.contains(where: { $0 == 2 })
+            && dict.values.contains(where: { $0 == 1 })
+    }
+
+    func isFullHouse(jIsJoker: Bool) -> Bool {
+        let dict = self.dict(jIsJoker: jIsJoker)
+        return dict.count == 2
+            && dict.values.contains(where: { $0 == 3 })
+            && dict.values.contains(where: { $0 == 2 })
+            && !dict.values.contains(where: { $0 == 1 })
+    }
+
+    func threeAreEqual(jIsJoker: Bool) -> Bool {
+        let dict = self.dict(jIsJoker: jIsJoker)
+        return dict.count == 3
+            && dict.values.contains(where: { $0 == 3 })
+            && !dict.values.contains(where: { $0 == 2 })
+            && dict.values.contains(where: { $0 == 1 })
+    }
+
+    func twoPairs(jIsJoker: Bool) -> Bool {
+        let dict = self.dict(jIsJoker: jIsJoker)
+        return dict.count == 3
+            && !dict.values.contains(where: { $0 == 3 })
+            && dict.values.contains(where: { $0 == 2 })
+            && dict.values.contains(where: { $0 == 1 })
+    }
+
+    func twoAreEqual(jIsJoker: Bool) -> Bool {
+        let dict = self.dict(jIsJoker: jIsJoker)
+        return dict.count == 4
+    }
+
+    func allAreDifferent(jIsJoker: Bool) -> Bool {
+        let dict = self.dict(jIsJoker: jIsJoker)
+        return dict.count == 5
+    }
+}
+
+private func camelPokerValues(jIsJoker: Bool = false) -> [Character: Int] {
+    [
+        "A": 13,
+        "K": 12,
+        "Q": 11,
+        "J": jIsJoker ? 0 : 10,
+        "T": 9,
+        "9": 8,
+        "8": 7,
+        "7": 6,
+        "6": 5,
+        "5": 4,
+        "4": 3,
+        "3": 2,
+        "2": 1,
+    ]
+}
+
+extension Array: Comparable where Element == Int {
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        zip(lhs, rhs)
+            .lazy
+            .filter { $0.0 != $0.1 }
+            .map { $0.0 < $0.1 }
+            .first
+        ?? false
+    }
+}
--- Tests/Day7Tests/Day7Tests.swift
+++ Tests/Day7Tests/Day7Tests.swift
@@ -2,15 +2,120 @@ import XCTest
 @testable import Day7
 
 final class Day7Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        32T3K 765
+        T55J5 684
+        KK677 28
+        KTJJT 220
+        QQQJA 483
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        let hands = Hands(input)
+        XCTAssertEqual(hands.totalWinnings, 6440)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let hands = Hands(input)
+        XCTAssertEqual(hands.totalWinnings, 250946742)
+        print("Part 1:", hands.totalWinnings)
+    }
+
+    func testPart2Example() {
+        let input = """
+        32T3K 765
+        T55J5 684
+        KK677 28
+        KTJJT 220
+        QQQJA 483
+        """
+
+        let hands = Hands(input, jIsJoker: true)
+        XCTAssertEqual(hands.totalWinnings, 5905)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        let hands = Hands(input, jIsJoker: true)
+        XCTAssertEqual(hands.totalWinnings, 251824095)
+        print("Part 2:", hands.totalWinnings)
+    }
+
+    func testHandSorting() {
+        XCTAssert(Hand("T55J5") > Hand("KK677"))
+        XCTAssert(Hand("KK677") < Hand("T55J5"))
+        XCTAssert(Hand("QQQJA") > Hand("KTJJT"))
+        XCTAssert(Hand("KTJJT") < Hand("QQQJA"))
+        XCTAssert(Hand("T55J5") < Hand("QQQJA"))
+        XCTAssert(Hand("QQQJA") > Hand("T55J5"))
+        XCTAssert(Hand("KK677") > Hand("KTJJT"))
+        XCTAssert(Hand("KTJJT") < Hand("KK677"))
+        XCTAssert(Hand("32T3K") < Hand("QQQJA"))
+        XCTAssert(Hand("QTQQJ") < Hand("JJJJJ"))
+        XCTAssert(Hand("QTQQJ") < Hand("KTKKJ"))
+        XCTAssert(Hand("QQQQJ") < Hand("KKKKQ"))
+        XCTAssert(Hand("QQQQJ") < Hand("QQQQK"))
+        XCTAssert(Hand("32T3K") < Hand("32Q3K"))
+        XCTAssert(Hand("32T3K") < Hand("32T3A"))
+
+        let hands = [
+            "32T3K",
+            "T55J5",
+            "KK677",
+            "KTJJT",
+            "QQQJA",
+        ].map { Hand($0) }
+
+        XCTAssertEqual(hands.sorted(by: <), [
+            Hand("32T3K"),
+            Hand("KTJJT"),
+            Hand("KK677"),
+            Hand("T55J5"),
+            Hand("QQQJA"),
+        ])
+
+        let hands2 = [
+            "35354",
+            "75858",
+            "2A2AT",
+            "78278",
+            "2QQJ2",
+            "94848",
+            "JJ299",
+        ].map { Hand($0) }
+
+        XCTAssertEqual(hands2.sorted(by: <), [
+            Hand("2QQJ2"),
+            Hand("2A2AT"),
+            Hand("35354"),
+            Hand("75858"),
+            Hand("78278"),
+            Hand("94848"),
+            Hand("JJ299"),
+        ])
+    }
+
+    func testHandParsing() {
+        let hands = Hands(input).hands.map(\.hand)
+
+        let dict = Dictionary(grouping: hands) { $0.matchType }
+
+        for (type, values) in dict {
+            switch type {
+            case .fiveOfAKind:
+                XCTAssert(values.allSatisfy { Set($0.cards).count == 1 })
+            case .fourOfAKind:
+                XCTAssert(values.allSatisfy { Set($0.cards).count == 2 })
+            case .fullHouse:
+                XCTAssert(values.allSatisfy { Set($0.cards).count == 2 })
+            case .threeOfAKind:
+                XCTAssert(values.allSatisfy { Set($0.cards).count == 3 })
+            case .twoPair:
+                XCTAssert(values.allSatisfy { Set($0.cards).count == 3 })
+            case .onePair:
+                XCTAssert(values.allSatisfy { Set($0.cards).count == 4 })
+            case .highCard:
+                XCTAssert(values.allSatisfy { Set($0.cards).count == 5 })
+            }
+        }
     }
 }
