ed16fe2e13aafa6285c8520ce16594fda6d2cb99
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-08 07:28:30 +0100

    Solve Day 8

--- Sources/Day8/Day8.swift
+++ Sources/Day8/Day8.swift
@@ -1,1 +1,77 @@
+import AOCAlgorithms
 
+struct Network {
+    struct Node: Identifiable {
+        let id: String
+        let left: Node.ID
+        let right: Node.ID
+
+        init(_ input: Substring) {
+            let pattern = #/(.{3}) = \((.{3}), (.{3})\)/#
+            guard let m = input.firstMatch(of: pattern) else { fatalError() }
+            id = String(m.output.1)
+            left = String(m.output.2)
+            right = String(m.output.3)
+        }
+    }
+
+    enum Instruction {
+        case left, right
+
+        init?(c: Character) {
+            switch c {
+            case "L":
+                self = .left
+            case "R":
+                self = .right
+            default:
+                return nil
+            }
+        }
+    }
+
+    var instructions: [Instruction]
+    var nodes: [Node.ID: Node]
+
+    init(_ input: String) {
+        let sections = input.split(separator: "\n\n")
+        instructions = sections[0].compactMap(Instruction.init)
+        let nodes = sections[1].split(separator: "\n").map(Node.init)
+        self.nodes = nodes.reduce(into: [:]) {
+            $0[$1.id] = $1
+        }
+    }
+    
+    var stepsToGoal: Int {
+        return stepsToGoal(of: "AAA", predicate: { $0 == "ZZZ" })
+    }
+
+    var stepsToGoalAsGhost: Int {
+        let currentNodes: [Node.ID] = nodes.keys.filter { $0.hasSuffix("A") }
+
+        let individualSteps = currentNodes.map {
+            return stepsToGoal(of: $0, predicate: { $0.hasSuffix("Z") })
+        }
+
+        return individualSteps.reduce(1) {
+            lcm($0, $1)
+        }
+    }
+
+    private func stepsToGoal(of node: Node.ID, predicate: (Node.ID) -> Bool) -> Int {
+        var steps = 0
+        var currentNode = node
+        while !predicate(currentNode) {
+            defer { steps += 1 }
+            let instruction = instructions[steps % instructions.count]
+            let nextNode = nodes[currentNode]!
+            switch instruction {
+            case .left:
+                currentNode = nextNode.left
+            case .right:
+                currentNode = nextNode.right
+            }
+        }
+        return steps
+    }
+}
--- Tests/Day8Tests/Day8Tests.swift
+++ Tests/Day8Tests/Day8Tests.swift
@@ -2,15 +2,59 @@ import XCTest
 @testable import Day8
 
 final class Day8Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input1 = """
+        RL
 
-    func testPart1() {
-        print("Part 1:")
+        AAA = (BBB, CCC)
+        BBB = (DDD, EEE)
+        CCC = (ZZZ, GGG)
+        DDD = (DDD, DDD)
+        EEE = (EEE, EEE)
+        GGG = (GGG, GGG)
+        ZZZ = (ZZZ, ZZZ)
+        """
+
+        let network1 = Network(input1)
+        XCTAssertEqual(network1.stepsToGoal, 2)
+
+        let input2 = """
+        LLR
+
+        AAA = (BBB, BBB)
+        BBB = (AAA, ZZZ)
+        ZZZ = (ZZZ, ZZZ)
+        """
+
+        let network2 = Network(input2)
+        XCTAssertEqual(network2.stepsToGoal, 6)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let network = Network(input)
+        print("Part 1:", network.stepsToGoal)
+    }
+
+    func testPart2Example() {
+        let input = """
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+        """
+        
+        let network = Network(input)
+        XCTAssertEqual(network.stepsToGoalAsGhost, 6)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        let network = Network(input)
+        print("Part 2:", network.stepsToGoalAsGhost)
     }
 }
