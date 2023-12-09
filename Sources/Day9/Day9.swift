39fbb197907edbd7e346eabda8091d1ec056c2cf
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-09 07:29:43 +0100

    Solve Day 9

--- Sources/Day9/Day9.swift
+++ Sources/Day9/Day9.swift
@@ -1,1 +1,57 @@
+struct SingleSequence {
+    var values: [Int]
 
+    private var isAllZeroes: Bool {
+        values.allSatisfy { $0 == 0 }
+    }
+
+    private var subsequence: SingleSequence {
+        let newValues = zip(values, values.dropFirst()).map { $1 - $0 }
+        return SingleSequence(values: newValues)
+    }
+
+    var extrapolatedValue: Int {
+        let subsequence = self.subsequence
+        if subsequence.isAllZeroes {
+            return values.last!
+        } else {
+            return values.last! + subsequence.extrapolatedValue
+        }
+    }
+
+    var backwardsExtrapolatedValue: Int {
+        let subsequence = self.subsequence
+
+        if subsequence.isAllZeroes {
+            return values.first!
+        } else {
+            return values.first! - subsequence.backwardsExtrapolatedValue
+        }
+    }
+}
+
+extension SingleSequence {
+    init(_ input: Substring) {
+        values = input.split(separator: " ").compactMap { Int($0) }
+    }
+}
+
+struct Sequences {
+    let sequences: [SingleSequence]
+
+    init(_ input: String) {
+        sequences = input.split(separator: "\n").map(SingleSequence.init)
+    }
+
+    var sumOfExtrapolatedValues: Int {
+        sequences.reduce(0) {
+            $0 + $1.extrapolatedValue
+        }
+    }
+
+    var sumOfBackwardsExtrapolatedValues: Int {
+        sequences.reduce(0) {
+            $0 + $1.backwardsExtrapolatedValue
+        }
+    }
+}
--- Tests/Day9Tests/Day9Tests.swift
+++ Tests/Day9Tests/Day9Tests.swift
@@ -2,15 +2,35 @@ import XCTest
 @testable import Day9
 
 final class Day9Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        let sequences = Sequences(input)
+        XCTAssertEqual(sequences.sumOfExtrapolatedValues, 114)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let sequences = Sequences(input)
+        print("Part 1:", sequences.sumOfExtrapolatedValues)
+    }
+
+    func testPart2Example() {
+        let input = """
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
+        """
+
+        let sequences = Sequences(input)
+        XCTAssertEqual(sequences.sumOfBackwardsExtrapolatedValues, 2)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        let sequences = Sequences(input)
+        print("Part 2:", sequences.sumOfBackwardsExtrapolatedValues)
     }
 }
