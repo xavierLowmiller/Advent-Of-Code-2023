a09da8b4865b895c26850c8bc211c326584e29fe
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-11 07:51:35 +0100

    Solve Day 11

--- Sources/Day11/Day11.swift
+++ Sources/Day11/Day11.swift
@@ -1,1 +1,48 @@
+struct GalaxyMap {
+    struct Point: Hashable {
+        let x, y: Int
+        func distance(to other: Point) -> Int {
+            abs(self.x - other.x) + abs(self.y - other.y)
+        }
+    }
 
+    let galaxies: Set<Point>
+
+    init(_ input: String, increment: Int = 2) {
+        let lines = input.split(separator: "\n").map(Array.init)
+
+        var galaxies: Set<Point> = []
+        var y = 0
+        for line in lines {
+            var x = 0
+            for (index, c) in line.enumerated() {
+                if c == "#" {
+                    galaxies.insert(Point(x: x, y: y))
+                }
+
+                x += lines.allSatisfy({ $0[index] == "." }) ? increment : 1
+            }
+
+            y += line.allSatisfy({ $0 == "." }) ? increment : 1
+        }
+
+        self.galaxies = galaxies
+    }
+
+    var sumOfDistancesBetweenGalaxies: Int {
+        let galaxies = Array(self.galaxies)
+
+        return galaxies.enumerated().reduce(into: 0) {
+            let (index, point) = $1
+            for p in galaxies.dropFirst(index + 1) {
+                $0 += p.distance(to: point)
+            }
+        }
+    }
+}
+
+private extension String {
+    var rows: [[Character]] {
+        self.split(separator: "\n").map(Array.init)
+    }
+}
--- Tests/Day11Tests/Day11Tests.swift
+++ Tests/Day11Tests/Day11Tests.swift
@@ -2,15 +2,52 @@ import XCTest
 @testable import Day11
 
 final class Day11Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        ...#......
+        .......#..
+        #.........
+        ..........
+        ......#...
+        .#........
+        .........#
+        ..........
+        .......#..
+        #...#.....
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        let map = GalaxyMap(input)
+        XCTAssertEqual(map.sumOfDistancesBetweenGalaxies, 374)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let map = GalaxyMap(input)
+        print("Part 1:", map.sumOfDistancesBetweenGalaxies)
+    }
+
+    func testPart2Example() {
+        let input = """
+        ...#......
+        .......#..
+        #.........
+        ..........
+        ......#...
+        .#........
+        .........#
+        ..........
+        .......#..
+        #...#.....
+        """
+
+        let map1 = GalaxyMap(input, increment: 10)
+        XCTAssertEqual(map1.sumOfDistancesBetweenGalaxies, 1030)
+
+        let map2 = GalaxyMap(input, increment: 100)
+        XCTAssertEqual(map2.sumOfDistancesBetweenGalaxies, 8410)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        let map = GalaxyMap(input, increment: 1000000)
+        print("Part 2:", map.sumOfDistancesBetweenGalaxies)
     }
 }
