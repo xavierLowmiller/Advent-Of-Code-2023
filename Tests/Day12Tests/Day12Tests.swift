c52c4387ce138ca0829bc378362c7347e86f9d77
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-12 19:39:26 +0100

    Solve Day 12

--- Tests/Day12Tests/Day12Tests.swift
+++ Tests/Day12Tests/Day12Tests.swift
@@ -2,15 +2,55 @@ import XCTest
 @testable import Day12
 
 final class Day12Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        ???.### 1,1,3
+        .??..??...?##. 1,1,3
+        ?#?#?#?#?#?#?#? 1,3,1,6
+        ????.#...#... 4,1,1
+        ????.######..#####. 1,6,5
+        ?###???????? 3,2,1
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        XCTAssertEqual("???.### 1,1,3".possibleArrangements(), 1)
+        XCTAssertEqual(".??..??...?##. 1,1,3".possibleArrangements(), 4)
+        XCTAssertEqual("?#?#?#?#?#?#?#? 1,3,1,6".possibleArrangements(), 1)
+        XCTAssertEqual("????.#...#... 4,1,1".possibleArrangements(), 1)
+        XCTAssertEqual("????.######..#####. 1,6,5".possibleArrangements(), 4)
+        XCTAssertEqual("?###???????? 3,2,1".possibleArrangements(), 10)
+
+        let arrangements = input.possibleArrangements()
+        XCTAssertEqual(arrangements, 21)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let arrangements = input.possibleArrangements()
+        print("Part 1:", arrangements)
+    }
+
+    func testPart2Example() {
+        let input = """
+        ???.### 1,1,3
+        .??..??...?##. 1,1,3
+        ?#?#?#?#?#?#?#? 1,3,1,6
+        ????.#...#... 4,1,1
+        ????.######..#####. 1,6,5
+        ?###???????? 3,2,1
+        """
+
+        XCTAssertEqual("???.### 1,1,3".possibleArrangements(repetitions: 5), 1)
+        XCTAssertEqual(".??..??...?##. 1,1,3".possibleArrangements(repetitions: 5), 16384)
+        XCTAssertEqual("?#?#?#?#?#?#?#? 1,3,1,6".possibleArrangements(repetitions: 5), 1)
+        XCTAssertEqual("????.#...#... 4,1,1".possibleArrangements(repetitions: 5), 16)
+        XCTAssertEqual("????.######..#####. 1,6,5".possibleArrangements(repetitions: 5), 2500)
+        XCTAssertEqual("?###???????? 3,2,1".possibleArrangements(repetitions: 5), 506250)
+
+        let arrangements = input.possibleArrangements(repetitions: 5)
+        XCTAssertEqual(arrangements, 525152)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        let arrangements = input.possibleArrangements(repetitions: 5)
+        print("Part 2:", arrangements)
     }
 }
