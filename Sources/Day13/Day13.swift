2ea59a94dfa4f700cbaed4b43775d0a1ca20779c
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-13 21:31:51 +0100

    Solve Day 13

--- Sources/Day13/Day13.swift
+++ Sources/Day13/Day13.swift
@@ -1,1 +1,53 @@
+struct Patterns {
+    let patterns: [Substring]
 
+    init(_ input: String) {
+        patterns = input.split(separator: "\n\n")
+    }
+
+    func reflectionSummary(maxDiff: Int) -> Int {
+        patterns.reduce(0) { $0 + $1.reflectionScore(maxDiff: maxDiff) }
+    }
+}
+
+extension StringProtocol {
+    func reflectionScore(maxDiff: Int) -> Int {
+        symmetryIndex(of: rows, maxDiff: maxDiff).map { $0 * 100 }
+        ?? symmetryIndex(of: columns, maxDiff: maxDiff)
+        ?? 0
+    }
+
+    var rows: [[Character]] {
+        self.split(separator: "\n").map { Array($0) }
+    }
+
+    var columns: [[Character]] {
+        var columns: [[Character]] = Array(repeating: [], count: rows[0].count)
+        for row in rows {
+            for (x, c) in row.enumerated() {
+                columns[x].append(c)
+            }
+        }
+        return columns
+    }
+
+    func symmetryIndex(of chars: [[Character]], maxDiff: Int) -> Int? {
+
+        for index in chars.indices.dropLast() {
+            let totalDifference = zip(chars[...index].reversed(), chars[(index + 1)...])
+                .reduce(0) { $0 + $1.0.hammingDistance(to: $1.1) }
+
+            if totalDifference == maxDiff {
+                return index + 1
+            }
+        }
+
+        return nil
+    }
+}
+
+private extension [Character] {
+    func hammingDistance(to other: Self) -> Int {
+        zip(self, other).reduce(0) { $0 + ($1.0 == $1.1 ? 0 : 1) }
+    }
+}
--- Tests/Day13Tests/Day13Tests.swift
+++ Tests/Day13Tests/Day13Tests.swift
@@ -2,15 +2,73 @@ import XCTest
 @testable import Day13
 
 final class Day13Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
 
-    func testPart1() {
-        print("Part 1:")
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
+        """
+
+        let blocks = input.split(separator: "\n\n")
+
+        XCTAssertEqual(blocks[0].symmetryIndex(of: blocks[0].rows, maxDiff: 0), nil)
+        XCTAssertEqual(blocks[0].symmetryIndex(of: blocks[0].columns, maxDiff: 0), 5)
+        XCTAssertEqual(blocks[1].symmetryIndex(of: blocks[1].rows, maxDiff: 0), 4)
+        XCTAssertEqual(blocks[1].symmetryIndex(of: blocks[1].columns, maxDiff: 0), nil)
+
+        let patterns = Patterns(input)
+        XCTAssertEqual(patterns.reflectionSummary(maxDiff: 0), 405)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let patterns = Patterns(input)
+        print("Part 1:", patterns.reflectionSummary(maxDiff: 0))
+    }
+
+    func testPart2Example() {
+        let input = """
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
+
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
+        """
+
+        let blocks = input.split(separator: "\n\n")
+
+        XCTAssertEqual(blocks[0].symmetryIndex(of: blocks[0].rows, maxDiff: 1), 3)
+        XCTAssertEqual(blocks[0].symmetryIndex(of: blocks[0].columns, maxDiff: 1), nil)
+        XCTAssertEqual(blocks[1].symmetryIndex(of: blocks[1].rows, maxDiff: 1), 1)
+        XCTAssertEqual(blocks[1].symmetryIndex(of: blocks[1].columns, maxDiff: 1), nil)
+
+        let patterns = Patterns(input)
+        XCTAssertEqual(patterns.reflectionSummary(maxDiff: 1), 400)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        let patterns = Patterns(input)
+        print("Part 2:", patterns.reflectionSummary(maxDiff: 1))
     }
 }
