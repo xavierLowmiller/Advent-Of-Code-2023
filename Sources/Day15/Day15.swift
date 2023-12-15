d7ce718f7bcc37a7dfe027848a6581e4dc4dac5a
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-15 07:47:53 +0100

    Solve Day 15

--- Sources/Day15/Day15.swift
+++ Sources/Day15/Day15.swift
@@ -1,1 +1,48 @@
+extension StringProtocol {
+    var holidayHash: Int {
+        var value = 0
+        for ascii in self.compactMap(\.asciiValue) {
+            value += Int(ascii)
+            value *= 17
+            value %= 256
+        }
+        return value
+    }
+}
 
+func focusingPower(of input: String) -> Int {
+
+    struct Lens {
+        let label: Substring
+        var focalLength: Int
+    }
+    var map: [Int: [Lens]] = [:]
+
+    for instruction in input.split(separator: ",") {
+
+        if instruction.contains("-") {
+            let label = instruction.split(separator: "-")[0]
+            map[label.holidayHash]?.removeAll(where: { $0.label == label })
+        } else {
+            let label = instruction.split(separator: "=")[0]
+            let focalLength = Int(instruction.suffix(1))!
+
+            var list = map[label.holidayHash, default: []]
+            if let index = list.firstIndex(where: { $0.label == label }) {
+                list[index].focalLength = focalLength
+            } else {
+                list.append(Lens(label: label, focalLength: focalLength))
+            }
+            map[label.holidayHash] = list
+        }
+    }
+
+    return map.keys.sorted().reduce(0) {
+        let lenses = map[$1]!
+        let factor = $1 + 1
+
+        return $0 + lenses.enumerated().reduce(0) {
+            $0 + factor * ($1.offset + 1) * $1.element.focalLength
+        }
+    }
+}
--- Tests/Day15Tests/Day15Tests.swift
+++ Tests/Day15Tests/Day15Tests.swift
@@ -2,15 +2,23 @@ import XCTest
 @testable import Day15
 
 final class Day15Tests: XCTestCase {
-    func testPart1Example() {}
-
-    func testPart1() {
-        print("Part 1:")
+    func testPart1Example() {
+        XCTAssertEqual("HASH".holidayHash, 52)
+        let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+        XCTAssertEqual(input.split(separator: ",").reduce(0) { $0 + $1.holidayHash }, 1320)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        let result = input.split(separator: ",").reduce(0) { $0 + $1.holidayHash }
+        print("Part 1:", result)
+    }
+
+    func testPart2Example() {
+        let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+        XCTAssertEqual(focusingPower(of: input), 145)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        print("Part 2:", focusingPower(of: input))
     }
 }
