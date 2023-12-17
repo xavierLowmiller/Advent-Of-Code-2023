228d9c187d0e1fb2563e0417e62bdf5013619928
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-17 21:59:18 +0100

    Solve Day 17

--- Sources/Day17/Day17.swift
+++ Sources/Day17/Day17.swift
@@ -1,1 +1,106 @@
+import AOCAlgorithms
 
+private struct Point: Hashable {
+    var x, y: Int
+
+    func distance(to other: Point) -> Int {
+        abs(x - other.x) + abs(y - other.y)
+    }
+
+    func nextSteps(
+        for history: [Point],
+        range: Range<Int>,
+        target: Point
+    ) -> Set<Point> {
+
+        let allSteps: Set<Point> = [
+            Point(x: x, y: y - 1),
+            Point(x: x, y: y + 1),
+            Point(x: x - 1, y: y),
+            Point(x: x + 1, y: y)
+        ]
+
+        guard let last = history.last else { return allSteps }
+
+        let diff = (x: x - last.x, y: y - last.y)
+
+        let toCheck = history + [self]
+
+        let streakLength = zip(toCheck, toCheck.dropFirst()).reversed()
+            .map({ (x: $1.x - $0.x, y: $1.y - $0.y) })
+            .prefix(while: { $0 == diff }).count
+
+        switch streakLength {
+        // Below range minimum: Continue straight
+        case ..<range.lowerBound:
+            let next = Point(x: x + diff.x, y: y + diff.y)
+            if next == target, range.lowerBound != streakLength + 1 {
+                return []
+            } else {
+                return [next]
+            }
+
+        // Upper end of range reached: Make turn
+        case range.upperBound... where diff.x == 0:
+            return Set([Point(x: x + 1, y: y), Point(x: x - 1, y: y)]).subtracting([target])
+        case range.upperBound... where diff.y == 0:
+            return Set([Point(x: x, y: y + 1), Point(x: x, y: y - 1)]).subtracting([target])
+
+        // Within valid range: Continue or make turn
+        default:
+            var next = allSteps.subtracting([last])
+            // Can only reach target if still within range
+            if next.contains(target) {
+                if target == Point(x: x + diff.x, y: y + diff.y) {
+                    if range.lowerBound...range.upperBound ~= streakLength + 1 {
+                        return next
+                    } else {
+                        next.remove(target)
+                    }
+                } else {
+                    if range.lowerBound != 0 {
+                        next.remove(target)
+                    }
+                }
+            }
+            return next
+        }
+    }
+}
+
+private struct PointAndHistory: Hashable {
+    var point: Point
+    var history: [Point] = []
+
+    func possibleSteps(within validRange: Set<Point>, range: Range<Int>, target: Point) -> Set<PointAndHistory> {
+        var history = history
+
+        let next = point.nextSteps(for: history, range: range, target: target)
+
+        if history.count == range.upperBound + 1 {
+            history.removeFirst()
+        }
+        history.append(point)
+
+        return Set(next
+            .intersection(validRange)
+            .map { PointAndHistory(point: $0, history: history) })
+    }
+}
+
+public func pathOfMinimumHeatLoss(_ input: String, range: Range<Int>) -> Int {
+    let values = input.split(separator: "\n").map { $0.compactMap(\.wholeNumberValue) }
+    let validPoints = Set(values.indices.flatMap { y in values[y].indices.map { Point(x: $0, y: y) } })
+    let start = Point(x: 0, y: 0)
+    let target = Point(x: values[0].indices.max()!, y: values.indices.max()!)
+
+    let path = aStar(
+        start: PointAndHistory(point: start),
+        goal: { $0.point == target },
+        cost: { values[$1.point.y][$1.point.x] },
+        heuristic: { target.distance(to: $0.point) },
+        neighbors: { $0.possibleSteps(within: validPoints, range: range, target: target) }
+    )
+
+    return path?.dropFirst().reduce(0) { $0 + values[$1.point.y][$1.point.x] } ?? 0
+}
--- Tests/Day17Tests/Day17Tests.swift
+++ Tests/Day17Tests/Day17Tests.swift
@@ -1,16 +1,62 @@
 import XCTest
-@testable import Day17
+import Day17
 
 final class Day17Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        2413432311323
+        3215453535623
+        3255245654254
+        3446585845452
+        4546657867536
+        1438598798454
+        4457876987766
+        3637877979653
+        4654967986887
+        4564679986453
+        1224686865563
+        2546548887735
+        4322674655533
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        XCTAssertEqual(pathOfMinimumHeatLoss(input, range: 0..<3), 102)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        print("Part 1:", pathOfMinimumHeatLoss(input, range: 0..<3))
+    }
+
+    func testPart2Example() {
+        let input1 = """
+        2413432311323
+        3215453535623
+        3255245654254
+        3446585845452
+        4546657867536
+        1438598798454
+        4457876987766
+        3637877979653
+        4654967986887
+        4564679986453
+        1224686865563
+        2546548887735
+        4322674655533
+        """
+
+        XCTAssertEqual(pathOfMinimumHeatLoss(input1, range: 4..<10), 94)
+
+        let input2 = """
+        111111111111
+        999999999991
+        999999999991
+        999999999991
+        999999999991
+        """
+
+        XCTAssertEqual(pathOfMinimumHeatLoss(input2, range: 4..<10), 71)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        print("Part 2:", pathOfMinimumHeatLoss(input, range: 4..<10))
     }
 }
