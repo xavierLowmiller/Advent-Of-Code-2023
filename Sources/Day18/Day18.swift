c46583cd99da3d34bc46324cb4e569fb41fef619
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-18 07:05:24 +0100

    Solve Day 18

--- Sources/Day18/Day18.swift
+++ Sources/Day18/Day18.swift
@@ -1,1 +1,67 @@
+public func enclosedLava(in input: String, isPart1: Bool) -> Int {
+    var points = [Vector(x: 0, y: 0)]
+    var borderLength: Int = 0
 
+    if isPart1 {
+        for line in input.split(separator: "\n") {
+            let direction = line.split(separator: " ")[0]
+            let distance = Int(line.split(separator: " ")[1])!
+
+            points.append(distance * offsets(c: direction) + points.last!)
+            borderLength += distance
+        }
+    } else {
+        for line in input.split(separator: "\n") {
+            let instruction = line.split(separator: " ")[2]
+                .dropFirst(2)
+                .dropLast(1)
+            let direction = instruction.suffix(1)
+            let distance = Int(instruction.dropLast(1), radix: 16)!
+
+            points.append(distance * offsets(c: direction) + points.last!)
+            borderLength += distance
+        }
+    }
+
+    return points.amount(with: borderLength)
+}
+
+private extension [Vector] {
+    func amount(with borderLength: Int) -> Int {
+        // shoelace formula
+        let area = zip(self, self.dropFirst()).reduce(into: 0) {
+            $0 += $1.0.x * $1.1.y - $1.1.x * $1.0.y
+        } / 2
+
+        // pick's theorem
+        return abs(area) - borderLength / 2 + 1 + borderLength
+    }
+}
+
+struct Vector: Hashable {
+    var x, y: Int
+
+    static func + (lhs: Vector, rhs: Vector) -> Vector {
+        Vector(
+            x: lhs.x + rhs.x,
+            y: lhs.y + rhs.y
+        )
+    }
+
+    static func * (factor: Int, vector: Vector) -> Vector {
+        Vector(
+            x: factor * vector.x,
+            y: factor * vector.y
+        )
+    }
+}
+
+private func offsets(c: Substring) -> Vector {
+    return switch c {
+    case "R", "0": Vector(x: 1, y: 0)
+    case "D", "1": Vector(x: 0, y: 1)
+    case "L", "2": Vector(x: -1, y: 0)
+    case "U", "3": Vector(x: 0, y: -1)
+    default: Vector(x: 9999, y: 9999)
+    }
+}
--- Tests/Day18Tests/Day18Tests.swift
+++ Tests/Day18Tests/Day18Tests.swift
@@ -2,15 +2,51 @@ import XCTest
 @testable import Day18
 
 final class Day18Tests: XCTestCase {
-    func testPart1Example() {}
-
-    func testPart1() {
-        print("Part 1:")
+    func testPart1Example() {
+        let input = """
+        R 6 (#70c710)
+        D 5 (#0dc571)
+        L 2 (#5713f0)
+        D 2 (#d2c081)
+        R 2 (#59c680)
+        D 2 (#411b91)
+        L 5 (#8ceee2)
+        U 2 (#caa173)
+        L 1 (#1b58a2)
+        U 2 (#caa171)
+        R 2 (#7807d2)
+        U 3 (#a77fa3)
+        L 2 (#015232)
+        U 2 (#7a21e3)
+        """
+        XCTAssertEqual(enclosedLava(in: input, isPart1: true), 62)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        print("Part 1:", enclosedLava(in: input, isPart1: true))
+    }
+
+    func testPart2Example() {
+        let input = """
+        R 6 (#70c710)
+        D 5 (#0dc571)
+        L 2 (#5713f0)
+        D 2 (#d2c081)
+        R 2 (#59c680)
+        D 2 (#411b91)
+        L 5 (#8ceee2)
+        U 2 (#caa173)
+        L 1 (#1b58a2)
+        U 2 (#caa171)
+        R 2 (#7807d2)
+        U 3 (#a77fa3)
+        L 2 (#015232)
+        U 2 (#7a21e3)
+        """
+        XCTAssertEqual(enclosedLava(in: input, isPart1: false), 952408144115)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        print("Part 2:", enclosedLava(in: input, isPart1: false))
     }
 }
