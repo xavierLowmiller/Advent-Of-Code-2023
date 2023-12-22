65650b4c8cd2580ba638e19be8b5b8abf63c670c
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-22 18:28:34 +0100

    Solve Day 22

--- Sources/Day22/Day22.swift
+++ Sources/Day22/Day22.swift
@@ -1,1 +1,121 @@
+struct Point: Hashable {
+    var x, y, z: Int
+}
 
+extension Point {
+    init(_ input: Substring) {
+        let coords = input.split(separator: ",").compactMap { Int($0) }
+        x = coords[0]
+        y = coords[1]
+        z = coords[2]
+    }
+}
+
+struct Brick: Hashable {
+    var from: Point
+    var to: Point
+
+    var lowerZ: Int {
+        min(from.z, to.z)
+    }
+
+    var upperZ: Int {
+        max(from.z, to.z)
+    }
+
+    mutating func moveDown(by z: Int) {
+        from.z -= z
+        to.z -= z
+    }
+
+    var xRange: ClosedRange<Int> {
+        from.x...to.x
+    }
+
+    var yRange: ClosedRange<Int> {
+        from.y...to.y
+    }
+
+    func hasOverlappingFootprint(with other: Brick) -> Bool {
+        other.xRange.overlaps(xRange) && other.yRange.overlaps(yRange)
+    }
+
+    func maxStepsToMoveDown(in bricks: some Sequence<Brick>) -> Int {
+        let overlappingBricks = bricks.filter { $0.hasOverlappingFootprint(with: self) }
+        assert(overlappingBricks.allSatisfy { $0.upperZ < lowerZ })
+        let maxZ = overlappingBricks.map(\.upperZ).max() ?? 0
+
+        let stepsToMoveDown = lowerZ - maxZ - 1
+        assert(stepsToMoveDown >= 0)
+        return stepsToMoveDown
+    }
+
+    mutating func moveDownAsFarAsPossible(among bricks: some Sequence<Brick>) {
+        moveDown(by: maxStepsToMoveDown(in: bricks))
+    }
+}
+
+extension Brick {
+    init(_ input: Substring) {
+        from = Point(input.split(separator: "~")[0])
+        to = Point(input.split(separator: "~")[1])
+    }
+}
+
+public func numberOfRemovableBricks(_ input: String) -> Int {
+    let bricks = input.split(separator: "\n").map(Brick.init)
+    return bricks
+        .compacted
+        .structurallyIrrelevantBricks
+        .count
+}
+
+public func bricksThatFallAfterDisintegrating(_ input: String) -> Int {
+    let bricks = input.split(separator: "\n").map(Brick.init).compacted
+
+    let removableBricks = Set(bricks.structurallyIrrelevantBricks)
+    let bricksThatCauseOtherBricksToFall = bricks.filter { !removableBricks.contains($0) }
+
+    return bricksThatCauseOtherBricksToFall
+        .reduce(0) { $0 + bricks.numberOfMovingBricks(whenRemoving: $1) }
+}
+
+extension [Brick] {
+    var compacted: [Brick] {
+        var fallenBricks: [Brick] = []
+        for var brick in sorted(by: { $0.lowerZ < $1.lowerZ }) {
+            brick.moveDownAsFarAsPossible(among: fallenBricks)
+            fallenBricks.append(brick)
+        }
+        return fallenBricks.sorted(by: { $0.lowerZ < $1.lowerZ })
+    }
+
+    var structurallyIrrelevantBricks: [Brick] {
+        filter { brick in
+            let bricksDirectlyAbove = filter { $0.lowerZ == brick.upperZ + 1 }
+            guard !bricksDirectlyAbove.isEmpty else { return true }
+
+            let bricksAtSameLevel = filter { $0.upperZ == brick.upperZ && $0 != brick }
+            return bricksDirectlyAbove.allSatisfy { aboveBrick in
+                bricksAtSameLevel.contains(where: { $0.hasOverlappingFootprint(with: aboveBrick) })
+            }
+        }
+    }
+
+    func numberOfMovingBricks(whenRemoving brick: Brick) -> Int {
+        let bricks = filter { $0 != brick }
+
+        var fallenBricks: [Brick] = []
+        var movingBricks = 0
+        for var brick in bricks {
+            let before = brick
+            brick.moveDownAsFarAsPossible(among: fallenBricks)
+            if before != brick {
+                movingBricks += 1
+            }
+            fallenBricks.append(brick)
+        }
+
+        return movingBricks
+    }
+}
--- Tests/Day22Tests/Day22Tests.swift
+++ Tests/Day22Tests/Day22Tests.swift
@@ -1,16 +1,40 @@
 import XCTest
-@testable import Day22
+import Day22
 
 final class Day22Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        1,0,1~1,2,1
+        0,0,2~2,0,2
+        0,2,3~2,2,3
+        0,0,4~0,2,4
+        2,0,5~2,2,5
+        0,1,6~2,1,6
+        1,1,8~1,1,9
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        XCTAssertEqual(numberOfRemovableBricks(input), 5)
     }
 
-    func testPart2Example() {}
+    func testPart1() {
+        print("Part 1:", numberOfRemovableBricks(input))
+    }
+
+    func testPart2Example() {
+        let input = """
+        1,0,1~1,2,1
+        0,0,2~2,0,2
+        0,2,3~2,2,3
+        0,0,4~0,2,4
+        2,0,5~2,2,5
+        0,1,6~2,1,6
+        1,1,8~1,1,9
+        """
+
+        XCTAssertEqual(bricksThatFallAfterDisintegrating(input), 7)
+    }
 
     func testPart2() {
-        print("Part 2:")
+        print("Part 2:", bricksThatFallAfterDisintegrating(input))
     }
 }
