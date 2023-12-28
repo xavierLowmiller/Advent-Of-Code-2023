abcd025d7b30a7efe080e120e89e97abb376ca21
Xaver Lohmüller <xaver.lohmueller@me.com>
2023-12-28 08:09:07 +0100

    Solve Day 25

--- Tests/Day25Tests/Day25Tests.swift
+++ Tests/Day25Tests/Day25Tests.swift
@@ -1,16 +1,28 @@
 import XCTest
-@testable import Day25
+import Day25
 
 final class Day25Tests: XCTestCase {
-    func testPart1Example() {}
+    func testPart1Example() {
+        let input = """
+        jqt: rhn xhk nvd
+        rsh: frs pzl lsr
+        xhk: hfx
+        cmg: qnr nvd lhk bvb
+        rhn: xhk bvb hfx
+        bvb: xhk hfx
+        pzl: lsr hfx nvd
+        qnr: nvd
+        ntq: jqt hfx bvb xhk
+        nvd: lhk
+        lsr: lhk
+        rzs: qnr cmg lsr rsh
+        frs: qnr lhk lsr
+        """
 
-    func testPart1() {
-        print("Part 1:")
+        XCTAssertEqual(scoreAfterDisconnectingGroups(input), 54)
     }
 
-    func testPart2Example() {}
-
-    func testPart2() {
-        print("Part 2:")
+    func testPart1() {
+        print("Part 1:", scoreAfterDisconnectingGroups(input))
     }
 }
